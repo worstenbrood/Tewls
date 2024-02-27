73d65c016ef6e18c9c088d8619e5e28570e92b80
Tim Horemans <tim.horemans@tenforce.com>
2024-02-27 01:34:24 +0100

    Optimalisation

--- Tewls/Windows/Crypt.cs
+++ Tewls/Windows/Crypt.cs
@@ -27,13 +27,17 @@ namespace Tewls.Windows
         [DllImport("Crypt32.dll", EntryPoint = "CryptUnprotectData", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Unicode, SetLastError = true)]
         private static extern bool CryptUnprotectData(DataBlob pDataIn, IntPtr ppszDataDescr, DataBlob pOptionalEntropy, IntPtr pvReserved, IntPtr pPromptStruct, uint dwFlags, DataBlob pDataOut);
 
-        public static string UnprotectData(DataBlob dataIn)
+        public static string UnprotectData(DataBlob dataIn, DataBlob optionalEntropy = null)
         {
             using (var buffer = new HGlobalBuffer())
             {
                 var dataOut = new DataBlob();
-                var result = CryptUnprotectData(dataIn, IntPtr.Zero, null, IntPtr.Zero, IntPtr.Zero, 0, dataOut);
+                var result = CryptUnprotectData(dataIn, IntPtr.Zero, optionalEntropy, IntPtr.Zero, IntPtr.Zero, 0, dataOut);
+                
+                // Free buffer
                 buffer.Buffer = dataOut.Data;
+                buffer.Size = (IntPtr) dataOut.Size;
+
                 if (!result)
                 {
                     throw new Win32Exception();
