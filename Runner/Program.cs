429d33918da8ab2e07275fb7c8bb7f07780466f4
Tim Horemans <tim.horemans@tenforce.com>
2024-03-22 01:53:14 +0100

    Update

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -1,28 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Tewls.Windows;
-using Tewls.Windows.NetApi;
-using Tewls.Windows.NetApi.Structures;
+using Tewls.Windows.Kernel;
+using Tewls.Windows.Kernel.Nt;
+using Tewls.Windows.PE;
 
 namespace Runner
 {
     internal class Program
     {
+        private delegate void Pointer(string[] argv);
+
         static void Main(string[] args)
         {
-            foreach(var info in NetServer.TransportEnum<ServerTransportInfo1>(null))
-            {
-                var t = info.Numberofvcs;
+            /*ConvertTools.ConvertCHeaderToEnum("C:\\Users\\tim.horemans\\Downloads\\test.txt", "C:\\Users\\tim.horemans\\Downloads\\test.cs",
+                "#define IMAGE_FILE_MACHINE_(?<name>(\\w+)).*(?<value>(0x[0-9a-fA-F]+))");*/
+
+            var process = new NativeProcess("jusched", ProcessAccessRights.AllAccess);
+            Console.WriteLine("Process ID: {0}", process.GetProcessId());
+
+            var start = DateTime.Now;
+
+            // 32/64
+            foreach (var module in process.GetModules())
+            {              
+                foreach (var export in process.GetExports(module.Address))
+                {
+                    Console.WriteLine("{0}: {1} ({2}) => 0x{3}", module.Name, export.Name ?? export.Ordinal.ToString(), export.Ordinal, export.Address.ToString("X"));
+                }
             }
 
+            Console.ReadKey();
 
-            foreach (var resource in WNet.EnumResources(WNet.ResourceScope.Remembered, WNet.ResourceType.Any, WNet.ResourceUsage.Connectable))
+            if (IntPtr.Size == 8)
             {
-                var parent = resource.GetNetworkInformation();
+                // 32 (Wow64)
+                foreach (var module in process.GetModulesWow64())
+                {
+                    foreach (var export in process.GetExportsWow64((uint)module.Address))
+                    {
+                        Console.WriteLine("{0}: {1} ({2}) => 0x{3}", module.Name, export.Name ?? export.Ordinal.ToString(), export.Ordinal, export.Address.ToString("X"));
+                    }
+                }
             }
+
+            Console.WriteLine("Time: {0}", (DateTime.Now - start).TotalMilliseconds);
+            Console.ReadKey();
         }
     }
 }
