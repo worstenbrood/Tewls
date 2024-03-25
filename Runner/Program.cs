1c0a4ee79aa145e8285d024a57ab739b5893ac16
Tim Horemans <tim.horemans@tenforce.com>
2024-03-25 11:06:54 +0100

    Update

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using IlDasm_CSharp;
+using System;
 using System.Linq;
 using Tewls.Windows.Kernel;
-using Tewls.Windows.Kernel.Nt;
-using Tewls.Windows.PE;
 
 namespace Runner
 {
@@ -13,7 +12,7 @@ namespace Runner
         static void Main(string[] args)
         {
             /*ConvertTools.ConvertCHeaderToEnum("C:\\Users\\tim.horemans\\Downloads\\test.txt", "C:\\Users\\tim.horemans\\Downloads\\test.cs",
-                "#define IMAGE_FILE_MACHINE_(?<name>(\\w+)).*(?<value>(0x[0-9a-fA-F]+))");*/
+                "#define IMAGE_SCN_(?<name>(\\w+)).*(?<value>(0x[0-9a-fA-F]+))");*/
 
             var process = new NativeProcess("jusched", ProcessAccessRights.AllAccess);
             Console.WriteLine("Process ID: {0}", process.GetProcessId());
@@ -21,24 +20,32 @@ namespace Runner
             var start = DateTime.Now;
 
             // 32/64
-            foreach (var module in process.GetModules())
-            {              
-                foreach (var export in process.GetExports(module.Address))
-                {
-                    Console.WriteLine("{0}: {1} ({2}) => 0x{3}", module.Name, export.Name ?? export.Ordinal.ToString(), export.Ordinal, export.Address.ToString("X"));
-                }
-            }
-
-            Console.ReadKey();
-
-            if (IntPtr.Size == 8)
+            foreach (var module in process.GetModules().Skip(1))
             {
-                // 32 (Wow64)
-                foreach (var module in process.GetModulesWow64())
+                foreach (var export in process.GetExports(module.Address).OrderBy(e => e.Ordinal))
                 {
-                    foreach (var export in process.GetExportsWow64((uint)module.Address))
+                    var b = process.ReadBytes(export.Address, 15);
+                    var len = b.GetILDasmLength(Environment.Is64BitProcess, 9);
+
+                    Console.WriteLine("{0}: {1} ({2}) => 0x{3} ({4})", module.Name, export.Name ?? export.Ordinal.ToString(), export.Ordinal, export.Address.ToString("X"),
+                        len);
+                }
+            }           
+
+            //Console.ReadKey();
+
+            if (Environment.Is64BitProcess)
+            {
+                // 32 (Wow64). Only need to do this if we are 64bit and the process is 32bit.
+                foreach (var module in process.GetModulesWow64().Skip(1))
+                {
+                    foreach (var export in process.GetExportsWow64((uint)module.Address).OrderBy(e => e.Ordinal))
                     {
-                        Console.WriteLine("{0}: {1} ({2}) => 0x{3}", module.Name, export.Name ?? export.Ordinal.ToString(), export.Ordinal, export.Address.ToString("X"));
+                        var b = process.ReadBytes(export.Address, 15);
+                        var len = b.GetILDasmLength(false, 5);
+
+                        Console.WriteLine("{0}: {1} ({2}) => 0x{3} ({4})", module.Name, export.Name ?? export.Ordinal.ToString(), export.Ordinal, export.Address.ToString("X"),
+                            len);
                     }
                 }
             }
