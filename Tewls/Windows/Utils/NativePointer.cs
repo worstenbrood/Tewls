805f391f8e4bad258332db3e67c51f8b3ff54401
Tim Horemans <tim.horemans@tenforce.com>
2024-05-31 00:25:59 +0200

    Added NativePointer

---
+++ Tewls/Windows/Utils/NativePointer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Tewls.Windows.Utils
+{
+    public class NativePointer : IDisposable
+    {
+        public IntPtr Pointer;
+        protected bool Disposed = false;
+
+        public static implicit operator IntPtr(NativePointer b) => b.Pointer;
+        public static implicit operator NativePointer(IntPtr b) => new NativePointer(b);
+
+        public NativePointer()
+        {
+        }
+
+        public NativePointer(IntPtr pointer)
+        {
+            Pointer = pointer;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            throw new NotImplementedException("Dispose"); 
+        }
+
+        public void Dispose()
+        {
+            if (!Disposed)
+            {
+                Dispose(true);
+                Disposed = true;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Pointer.GetHashCode();
+        }
+
+        ~NativePointer()
+        {
+            if (!Disposed)
+            {
+                Dispose(false);
+                Disposed = true;
+            }
+        }
+    }
+}
