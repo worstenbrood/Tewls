bbf068029c2d0ed9699a6d2ecc4e62b37ae88e4d
worstenbrood <worstenbrood@gmail.com>
2026-04-13 22:00:00 +0200

    Added missing files

---
+++ TewlKit/Core/Procedure.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+<<<<<<< TODO: Unmerged change from project 'TewlKit (net8.0)', Before:
+=======
+using TewlKit;
+using TewlKit.Core;
+using TewlKit.Core;
+using TewlKit.Core.Functions;
+>>>>>>> After
+
+namespace TewlKit.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Procedure<T>
+        where T : Delegate
+    {
+        /// <summary>
+        /// Delegate
+        /// </summary>
+        public readonly T Method;
+
+        /// <summary>
+        /// Memory address
+        /// </summary>
+        public readonly nint Address;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="d"></param>
+        public Procedure(T d)
+        {
+            Method = d;
+            // Get function pointer
+            Address = Marshal.GetFunctionPointerForDelegate(Method);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="d"></param>
+        public Procedure(nint d)
+        {
+            Address = d;
+            // Get delegate
+            Method = (T)Marshal.GetDelegateForFunctionPointer(d, typeof(T));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="f"></param>
+        public static implicit operator nint(Procedure<T> f) => f.Address;
+                
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="f"></param>
+        public static implicit operator T(Procedure<T> f) => f.Method;
+    }
+}
---
+++ TewlKit/Hooks/MessageBoxHook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TewlKit.Core;
+
+namespace TewlKit.Hooks
+{
+    public class MessageBoxHook : Hook<MessageBoxHook.MessageBox>
+    {
+        public delegate int MessageBox(IntPtr hwnd, string text, string caption, int type);
+
+        public int HookedMessageBox(IntPtr hwnd, string text, string caption, int type)
+        {
+            return Trampoline.Original.Method.Invoke(hwnd, text, "HOOKED: " + caption, type);
+        }
+
+        public MessageBoxHook(IntPtr procAddr) : base("kernel32.dll", "OpenProcess")
+        {
+            Trampoline = new Trampoline<MessageBox>(procAddr, new MessageBox(HookedMessageBox));
+        }
+    }
+}
